3588f079d714be9d12a7a2c01610696ac1310165
Web Kawsar <web.kawsarahmed@gmail.com>
2020-12-04 06:01:08 +0600

    done intro

--- src/Componenets/About/About.js
+++ src/Componenets/About/About.js
@@ -73,22 +73,22 @@ const About = () => {
                         <Grid className="animate__animated animate__bounceInUp" item md={12}>
                             <h3 className={classes.about}>About <span className={classes.highlighter}>Me</span></h3>
                             <Grid container>
-                                <Grid item md={7}>
+                                <Grid item md={12}>
                                     <p className={classes.myInfo}>Programming is my passion. I love to work with web technologies. Always try to grab the new features of technology. Last few years made myself skilled in the field of web application development. Expert in developing Single Page Application (SPA) using React JS. I am very much comfotable with JavaScript. I worked with different types of libraries and framework. Among of them I feel comfortable with React, Node, Express, MongoDB, Material UI etc</p>
                                 </Grid>
-                                <Grid className={classes.info} item md={5}>
-                                    {/* <p><span className={classes.highlighter}>Age : </span>24</p>
+                                {/* <Grid className={classes.info} item md={5}>
+                                    <p><span className={classes.highlighter}>Age : </span>24</p>
                                     <p><span className={classes.highlighter}>Residence : </span>Bangladesh</p>
                                     <p><span className={classes.highlighter}>Address : </span>MoulviBazar, Sylhet, Bangladesh</p>
                                     <p><span className={classes.highlighter}>Email : </span>web.kawsarahmed@gmail.com</p>
-                                    <p><span className={classes.highlighter}>Phone : </span>+880 1733 920 943</p> */}
+                                    <p><span className={classes.highlighter}>Phone : </span>+880 1733 920 943</p> 
 
-                                    {/* <Box>
+                                    <Box>
                                         <img className={classes.img} src={image} alt=""/>
-                                    </Box> */}
+                                    </Box> 
 
                                     
-                                </Grid>
+                                </Grid> */}
                             </Grid>
                         </Grid>
                         <Grid item md={12}>
